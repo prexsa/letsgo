10e7ecfee8733efb90848cc694b4a926c8e03616
VY-1 <107969625+VY-1@users.noreply.github.com>
2024-10-31 14:42:06 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask
+import json
+from bson import json_util, ObjectId
+
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, render_template, request, jsonify
+import os
+import pprint
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+#load .env file for MongoDB connection
+load_dotenv()
+
+client = MongoClient()
 
 app = Flask(__name__)
 
+# Create a new client and connect to the server
+client = MongoClient(os.environ.get("MONGODB_URI"), server_api=ServerApi('1'))
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+#create database instance in our app
+app.db = client.letsgo
+#create flight collection if not exist otherwise use it
+app.db.flight = app.db.flight
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    if request.method == "POST":
+        flight_content = request.get_json()
+        new_flight = { "name": flight_content["name"], "capacity": flight_content["capacity"], "routes": []}
+        flight_id = app.db.flight.insert_one(new_flight).inserted_id
+
+        return jsonify(flight_content)
+
+    flights = json_util.dumps(app.db.flight.find())
+    return json.loads(flights)
 
 
-if __name__ == '__main__':
-    app.run()
+app.run(debug=True)
